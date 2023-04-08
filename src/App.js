62ad5bff59a3f6dc6752a5a1ccdae2d58fb6c8b4
AlbertNotAlien <albert830207@gmail.com>
2023-04-08 20:10:04 +0800

    feat: Setup the porject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./reset.css";
+import styled from "@emotion/styled";
+
+const Container = styled.div``;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>123</Container>
     </div>
   );
 }
